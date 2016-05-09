f6cfc9d988405cbeaf5e3b2f31c37bec5baed560
Matthias Greil <matthias.greil@gmx.at>
2016-05-09 21:54:06 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string neuerPreis;
+            var MeinAuto = new Auto("Audi", "A4", 50000);
+            Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+            MeinAuto.UpdatePrice(10000);
+            Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+
+            /*Console.WriteLine("Geben Sie den neuen Preis ein: ");
+            neuerPreis = Console.ReadLine();
+            MeinAuto.SetGetPrice = Convert.ToDecimal(neuerPreis);
+            Console.WriteLine("Marke: {0} Modell: {1}, Preis: {2}", MeinAuto.m_marke, MeinAuto.m_modell, MeinAuto.SetGetPrice);*/
+        }
+    }
+}
