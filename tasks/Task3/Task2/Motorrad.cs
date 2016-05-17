dba413160235d0f4316c46385d1470a6d084c637
Matthias Greil <matthias.greil@gmx.at>
2016-05-17 19:14:12 +0200

    add Task3

---
+++ tasks/Task3/Task2/Motorrad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Motorrad : Fahrzeug
+    {
+        private decimal m_preis;
+        private string m_marke;
+        private string m_modell;
+
+        public Motorrad(string marke, string modell, decimal preis)
+        {
+            if (string.IsNullOrWhiteSpace(marke))
+                throw new Exception("Motorradmarke muss einen Namen haben");
+            if (string.IsNullOrWhiteSpace(modell))
+                throw new Exception("Modell muss einen Namen haben");
+            if (preis < 0)
+                throw new Exception("Preis darf nicht kleiner als 0 betragen");
+
+
+            m_marke = marke;
+            m_modell = modell;
+            m_preis = preis;
+        }
+
+        public void UpdatePrice(decimal Price)
+        {
+            if (Price < 0)
+                throw new Exception("Preis darf nicht kleiner als 0 betragen");
+            m_preis = Price;
+        }
+
+        public string Marke
+        {
+            get
+            {
+                return m_marke;
+            }
+        }
+
+        public string Modell
+        {
+            get
+            {
+                return m_modell;
+            }
+        }
+
+        public decimal Preis
+        {
+            get
+            {
+                return m_preis;
+            }
+        }
+
+    }
+}
---
+++ tasks/Task3/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* var MeinAuto = new Auto("Audi", "A4", 50000);
+             Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+             MeinAuto.UpdatePrice(10000);
+             Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+             */
+
+            var item = new Fahrzeug[]
+            {
+                new Auto("Audi", "A4", 50000),
+                new Auto("BMW", "3er", 40000),
+                new Motorrad("KTM", "ABC", 10000),
+            };
+
+            item[0].UpdatePrice(99999);
+
+            foreach(var x in item)
+            {
+                Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", x.Marke, x.Modell, x.Preis);
+            }
+
+        }
+    }
+}
