23e58e6ae7feaca29b67eadaac27df24f940c527
Matthias Greil <matthias.greil@gmx.at>
2016-05-23 20:47:14 +0200

    add Tast4

---
+++ tasks/Task4/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* var MeinAuto = new Auto("Audi", "A4", 50000);
+             Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+             MeinAuto.UpdatePrice(10000);
+             Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", MeinAuto.Marke, MeinAuto.Modell, MeinAuto.Preis);
+             */
+
+            var item = new Fahrzeug[]
+            {
+                new Auto("Audi", "A4", 50000),
+                new Auto("BMW", "3er", 40000),
+                new Motorrad("KTM", "ABC", 10000),
+            };
+
+            item[0].UpdatePrice(99999);
+
+            foreach(var x in item)
+            {
+                Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", x.Marke, x.Modell, x.Preis);
+            }
+
+            var settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+            
+            //store to file
+            var text = JsonConvert.SerializeObject(item, settings);
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var filename = Path.Combine(desktop, "Fahrzeuge.json");
+            File.WriteAllText(filename, text);
+
+            //read file
+            var textFromFile = File.ReadAllText(filename);
+            Console.WriteLine(textFromFile);
+
+            var itemsFromFile = JsonConvert.DeserializeObject<Fahrzeug[]>(textFromFile, settings);
+            foreach (var x in itemsFromFile)
+            {
+                Console.WriteLine("Marke: {0} Modell: {1} Preis: {2}", x.Marke, x.Modell, x.Preis);
+            }
+        }
+    }
+}
---
+++ tasks/Task4/Task2/Tests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task2
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void CanCreateAuto()
+        {
+            var x = new Auto("Audi", "A4", 50000);
+
+            Assert.IsTrue(x.Marke == "Audi");
+            Assert.IsTrue(x.Modell == "A4");
+            Assert.IsTrue(x.Preis == 50000);
+        }
+
+        [Test]
+        public void CanCreateMotorrad()
+        {
+            var x = new Motorrad("BMW", "ABC", 50000);
+
+            Assert.IsTrue(x.Marke == "BMW");
+            Assert.IsTrue(x.Modell == "ABC");
+            Assert.IsTrue(x.Preis == 50000);
+        }
+
+        [Test]
+        public void AutoWithEmptyMarke1()
+        {
+            Assert.Catch(() =>
+                {
+                    var x = new Auto(null, "A4", 50000);
+                });
+        }
+
+        [Test]
+        public void AutoWithEmptyMarke2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Auto("", "A4", 50000);
+            });
+        }
+
+        [Test]
+        public void MotorradWithEmptyMarke1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Motorrad(null, "ABC", 50000);
+            });
+        }
+
+        [Test]
+        public void MotorradWithEmptyMarke2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Motorrad("", "ABC", 50000);
+            });
+        }
+
+        [Test]
+        public void AutoWithEmptyModell1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Auto("Audi", null, 50000);
+            });
+        }
+
+        [Test]
+        public void AutoWithEmptyModell2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Auto("Audi", "", 50000);
+            });
+        }
+
+        [Test]
+        public void MotorradWithEmptyModell1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Motorrad("BMW", null, 50000);
+            });
+        }
+
+        [Test]
+        public void MotorradWithEmptyModell2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Motorrad("BMW", "", 50000);
+            });
+        }
+
+        
+
+    }
+}
